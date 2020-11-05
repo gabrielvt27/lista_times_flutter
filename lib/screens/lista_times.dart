c9a41fe7b5d6bdee090a64f75240af5c32918e4a
gabrielvt27 <tiburcio_gabriel@outlook.com>
2020-11-05 08:28:24 -0300

    Commit de teste

--- lib/screens/lista_times.dart
+++ lib/screens/lista_times.dart
@@ -10,7 +10,7 @@ class ListaTimes extends StatefulWidget {
 
 class _ListaTimesState extends State<ListaTimes> {
 
-  List _listaTimes = ["São Caetano", "Mauá"];
+  List _listaTimes = ["São Caetano", "Mauá", "São Bernardo"];
 
   @override
   Widget build(BuildContext context) {
